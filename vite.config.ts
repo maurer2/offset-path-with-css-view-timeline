811fe5010575c010344d909092223e2db2e0913f
Martin <mr.mmaurer@gmail.com>
2026-08-25 13:13:17 +0200

    Added experimental rust based react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  // experimental rust port without logging function atm: https://oxc.rs/blog/2026-08-18-react-compiler-support.html#vite
+  plugins: [react({ compiler: true }), svgr()],
   base: '/offset-path-with-css-view-timeline/',
 });

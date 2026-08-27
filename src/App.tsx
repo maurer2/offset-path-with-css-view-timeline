699950da1b3c09b468ec806ca5412e6ea8eac422
Martin <mr.mmaurer@gmail.com>
2026-08-27 21:46:51 +0200

    Looked into max-content-sizing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,19 @@ import './App.css';
 import CatSVG from '#assets/cat.svg?react';
 import { useResizeObserverWithSyncExternalStore } from '#hooks/useResizeObserverWithSyncExternalStore';
 
+import bookJSON from './assets/book.json' with { type: 'json' };
+
+type Book = {
+  title: string;
+  author: string;
+  chapters: {
+    title: string;
+    content: string;
+  }[];
+};
+
+const book = bookJSON satisfies Book;
+
 export function App() {
   const overlayRef = useRef<HTMLDivElement>(null);
   const size = useResizeObserverWithSyncExternalStore(overlayRef);
@@ -11,7 +24,21 @@ export function App() {
   return (
     <>
       <main className="main">
-        <h1>Page title</h1>
+        <h1>{book.title}</h1>
+        {/* <div className="chapters">
+          {book.chapters.map((chapter) => (
+            <section
+              className="chapter"
+              id={chapter.title}
+              key={chapter.title}
+            >
+              <header className="chapter-header">
+                <h2 className="chapter-title">{chapter.title}</h2>
+              </header>
+              <p className="chapter-content">{chapter.content}</p>
+            </section>
+          ))}
+        </div> */}
       </main>
 
       <div
